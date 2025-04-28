b893601a0f16d9eefd9faca0bc9bd441a0cbf887
campogiovannaabreu <giovanna.abreucampos1a@gmail.com>
2025-04-28 15:55:34 -0300

    Escreva um programa que leia 20 números do usuário e exiba quantos números são maiores que 10.

---
+++ Ex63.java
@@ -0,0 +1,21 @@
+/*
+9.   
+Escreva um programa que leia 20 números do usuário e exiba quantos números são maiores que 10.
+*/
+import java.util.Scanner;
+
+public class Ex63 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int maiorque = 0;
+        
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("Informe o número: ");
+            int numero = ler.nextInt(); 
+            if (numero > 10) {
+                maiorque++;
+            }
+        }
+            System.out.println("Total de números maiores que 10: " + maiorque);
+        }
+    }
