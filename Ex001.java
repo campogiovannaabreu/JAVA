9f7b60f4586aa45f7ddac3258aa7ead7c3a16a69
campogiovannaabreu <giovanna.abreucampos1a@gmail.com>
2025-04-30 07:40:19 -0300

    Exercicio 001 Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]

---
+++ Ex001.java
@@ -0,0 +1,17 @@
+/*
+Exercicio 001
+Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+*/
+import java.util.Scanner;
+public class Ex001
+{
+	public static void main(String[] args) {
+	    double numero;
+	    Scanner ler = new Scanner (System.in);
+	    
+		System.out.println("Informe um número:");
+		numero = ler.nextInt();
+		
+		System.out.println("O número informado foi:" + numero);
+	}
+}
